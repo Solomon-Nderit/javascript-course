c7cd9d82efab817e3bf400a2a6b9c51ba968e137
Solomon Nderitu Wahome <solomonstead@gmail.com>
2025-05-30 14:18:53 +0300

    Comparison operators

---
+++ comparison_operators.js
@@ -0,0 +1,22 @@
+let num1 = 14;
+let num2 = 10;
+
+const isNum1GreaterThanNum2 = num1 > num2; 
+
+console.log('Is num1 greater than num2?', isNum1GreaterThanNum2);
+
+/* Other comparison operators */
+const isNum1LessThanNum2 = num1 < num2;
+console.log('Is num1 less than num2?', isNum1LessThanNum2);
+const isNum1EqualToNum2 = num1 === num2;
+console.log('Is num1 equal to num2?', isNum1EqualToNum2);
+const isNum1NotEqualToNum2 = num1 !== num2;
+console.log('Is num1 not equal to num2?', isNum1NotEqualToNum2);
+const isNum1GreaterThanOrEqualToNum2 = num1 >= num2;
+console.log('Is num1 greater than or equal to num2?', isNum1GreaterThanOrEqualToNum2);
+const isNum1LessThanOrEqualToNum2 = num1 <= num2;
+console.log('Is num1 less than or equal to num2?', isNum1LessThanOrEqualToNum2);
+const isNum1StrictlyEqualToNum2 = num1 === num2;
+console.log('Is num1 strictly equal to num2?', isNum1StrictlyEqualToNum2);
+const isNum1StrictlyNotEqualToNum2 = num1 !== num2;
+console.log('Is num1 strictly not equal to num2?', isNum1StrictlyNotEqualToNum2);
