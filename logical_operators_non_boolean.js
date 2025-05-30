535270b49ca36ed319112ce5f002b4b2888a0993
Solomon Nderitu Wahome <solomonstead@gmail.com>
2025-05-30 14:39:22 +0300

    Logical Operators with non-boolean values

---
+++ logical_operators_non_boolean.js
@@ -0,0 +1,11 @@
+// In JS, the logical operators &&, ||, and ! can be used with non-boolean values.
+// The non-boolean values that are considered falsy in JavaScript include:
+// 0, "", null, undefined, NaN, and false.
+
+// Sample
+let value1 = 0;
+let value2 = "Hello";
+
+// Logical AND (&&) with non-boolean values
+let andResult = value1 && value2;
+console.log('Logical AND result:', andResult);
