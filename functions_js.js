9e00b487323dfefbe4cc1146aca6afc018d57114
Solomon Nderitu Wahome <solomonstead@gmail.com>
2025-05-30 14:12:16 +0300

    Learnt about functions (defined and undefined)

---
+++ functions_js.js
@@ -0,0 +1,15 @@
+/*Defined Function */
+function sayHi(name) {
+let greeting = 'Hi ' + name;
+return greeting;
+}
+
+sayHi("Solomon")
+
+/*Undefined Function */
+function sayHi(name) {
+let greeting = 'Hi ' + name;
+
+}
+
+sayHi("Solomon")
