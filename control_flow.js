09090b9341bcb3e578b401376ff9941e3d622782
Solomon Nderitu Wahome <solomonstead@gmail.com>
2025-05-30 15:49:07 +0300

    Switch_case

---
+++ control_flow.js
@@ -0,0 +1,12 @@
+let chocolatePrice = 2.5;
+let customerMoney = 10;
+
+let ability = customerMoney>=chocolatePrice;
+
+if (ability) {
+    console.log("You can buy the chocolate.");
+}
+
+else {
+  console.log('You cannot buy the chocolate.')
+}
---
+++ switch_case.js
@@ -0,0 +1,15 @@
+let role = 'software developer'
+
+switch(role) {
+  case 'Software developer':
+    console.log('Writes code');
+    break;
+  case 'Designer':
+    console.log("Makes User Interfaces");
+    break;
+  case 'Cloud Engineer':
+    console.log("Manages and Deploys Cloud Resources");
+    break;
+  default:
+    console.log("Works directly with customers");
+}
